eadacc4d678362a1285c5ca9858108eb4a56aa10
Khalid Rabiu Salis <khalidrabiusalis@gmail.com>
2024-10-18 15:06:26 +0100

    change path to frontend

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors({ origin: 'https://trash-away.vercel.app/support' })); // Allow requests from your frontend
+app.use(cors({ origin: 'https://trash-away.vercel.app' }));
 app.use(express.json()); 
 
 // Routes
